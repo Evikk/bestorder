0ea58501c4566e3a1099bdb5d2be8e3039b33869
evikk <evyatarm@gmail.com>
2020-07-10 16:09:14 +0300

    db reset

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todotest.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 
